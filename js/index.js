78b693821e2d9810cb64ef9e62691deb22e4223d
isaccsz <isaccruz47@outlook.com>
2022-09-08 23:34:04 -0300

    daily commit

--- js/index.js
+++ js/index.js
@@ -16,3 +16,15 @@ function inserirCalculo(){
     }
     
 }
+
+function turnOff(){
+    let fundo = document.getElementById('field');
+
+    if(fundo.style.background == 'white'){
+        fundo.style.background = 'black';
+        document.getElementById('off').innerHTML = 'ON'
+    }else{
+        fundo.style.background = 'white'
+        document.getElementById('off').innerHTML = 'OFF'
+    }
+}
