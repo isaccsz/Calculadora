86c00666aa18ab4d4d33e74cb6c37fefa5503018
isaccsz <isaccruz47@outlook.com>
2022-09-08 23:06:06 -0300

    daily commit

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ function inserirNumero(num){
     let numero = document.getElementById('field').innerHTML;
     document.getElementById('field').innerHTML = numero + num;
 }
+
+function limparNumero(){
+    document.getElementById('field').innerHTML = '';
+}
